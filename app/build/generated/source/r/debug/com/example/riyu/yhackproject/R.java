c7a5ead346f85c22bd2d731910b44c447d3ac337
ColinSz <kenavt@gmail.com>
2014-11-01 02:10:07 -0400

    Riyu forgot his #includes. Also began working on a rudimentary greeting app

--- app/build/generated/source/r/debug/com/example/riyu/yhackproject/R.java
+++ app/build/generated/source/r/debug/com/example/riyu/yhackproject/R.java
@@ -11,17 +11,34 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int widget_margin=0x7f050000;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int widget_margin=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int preview=0x7f020001;
     }
+    public static final class id {
+        public static final int action_settings=0x7f090004;
+        public static final int container=0x7f090000;
+        public static final int new_username=0x7f090002;
+        public static final int normal_text=0x7f090001;
+        public static final int submit_name_to_cloud=0x7f090003;
+    }
     public static final class layout {
-        public static final int appwidget=0x7f030000;
+        public static final int activity_settings=0x7f030000;
+        public static final int appwidget=0x7f030001;
+        public static final int fragment_settings=0x7f030002;
+    }
+    public static final class menu {
+        public static final int settings=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int enter_name=0x7f060002;
+        public static final int title_activity_settings=0x7f060003;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
--- app/src/main/java/com/example/riyu/yhackproject/AppWidgetProvider.java
+++ app/src/main/java/com/example/riyu/yhackproject/AppWidgetProvider.java
@@ -1,5 +1,14 @@
 package com.example.riyu.yhackproject;
 
+import android.content.Context;
+import android.appwidget.AppWidgetManager;
+
+import android.content.Intent;
+import android.app.PendingIntent;
+
+import android.widget.RemoteViews;
+
+
 /**
  * Created by Riyu on 10/31/14.
  */
@@ -12,16 +21,16 @@ public class AppWidgetProvider {
             int appWidgetId = appWidgetIds[i];
 
             // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, ExampleActivity.class);
+            Intent intent = new Intent(context, SettingsActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
-            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+            //RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+            //views.setOnClickPendingIntent(R.id.button, pendingIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            //appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 }
