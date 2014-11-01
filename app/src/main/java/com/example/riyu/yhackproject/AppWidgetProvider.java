34042b4331471c19186a13e93f51e8b05feba8c9
Riyu <riyu.banerjee@gmail.com>
2014-11-01 01:05:57 -0400

    Started creating a widget for the app

---
+++ app/src/main/java/com/example/riyu/yhackproject/AppWidgetProvider.java
@@ -0,0 +1,27 @@
+package com.example.riyu.yhackproject;
+
+/**
+ * Created by Riyu on 10/31/14.
+ */
+public class AppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, ExampleActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
